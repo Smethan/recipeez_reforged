9670f6a756fbc1a125cfa64996c9673773f294d4
smethan <ethanhargus@gmail.com>
2022-08-25 14:32:12 -0500

    fix to hopefully get it to compile on heroku

--- src/components/FileManagement.tsx
+++ src/components/FileManagement.tsx
@@ -1,30 +1,36 @@
-import { s3Config } from "../s3Config";
 import ReactS3Client from 'react-aws-s3-typescript';
 import React from "react";
+
+const s3Config = {
+    bucketName: process.env.bucket_name!,
+    region: process.env.region!,
+    accessKeyId: process.env.accessKeyId!,
+    secretAccessKey: process.env.secretAccessKey!,
+}
 
 const s3 = new ReactS3Client(s3Config)
 
 const uploadFile = async (file: File) => {
-    
+
 
     try {
         const res = await s3.uploadFile(file);
         console.log(res.location)
-        return(res.location)
-    } catch(exception) {
+        return (res.location)
+    } catch (exception) {
         console.log(exception)
-        return("Error")
+        return ("Error")
     }
 }
 
 const deleteFile = async (name: string) => {
     try {
         await s3.deleteFile(name);
-        return("File Deleted")
+        return ("File Deleted")
     } catch (exception) {
         console.log(exception)
-        return("Error")
+        return ("Error")
     }
 }
 
-export {uploadFile, deleteFile}
+export { uploadFile, deleteFile }
